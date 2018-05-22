3564ef3dc180db31dac8448f95abe532965c559d
aviau <alexandre@alexandreviau.net>
2018-05-22 03:10:51 -0400

    create TestAddPGPKey

--- internal/server/router/internal/endpoints/webpages/profile_test.go
+++ internal/server/router/internal/endpoints/webpages/profile_test.go
@@ -1,10 +1,15 @@
 package webpages_test
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
+	"salsa.debian.org/autodeb-team/autodeb/internal/pgp"
+	"salsa.debian.org/autodeb-team/autodeb/internal/pgp/pgptest"
 	"salsa.debian.org/autodeb-team/autodeb/internal/server/router/routertest"
 
 	"github.com/stretchr/testify/assert"
@@ -27,4 +32,38 @@ func TestProfileGetHandlerAuthenticated(t *testing.T) {
 
 	assert.Equal(t, http.StatusSeeOther, response.Result().StatusCode)
 	assert.NotContains(t, response.Body.String(), user.Username)
+}
+
+func TestAddPGPKeyPostHandler(t *testing.T) {
+	testRouter := routertest.SetupTest(t)
+
+	user := testRouter.Login()
+
+	keys, err := testRouter.App.GetUserPGPKeys(user.ID)
+	assert.NotNil(t, keys)
+	assert.Equal(t, 0, len(keys))
+	assert.NoError(t, err)
+
+	var proof bytes.Buffer
+	pgp.Clearsign(
+		strings.NewReader(testRouter.App.ExpectedPGPKeyProofText(user.ID)),
+		strings.NewReader(pgptest.TestKeyPrivate),
+		&proof,
+	)
+
+	form := &url.Values{}
+	form.Add("key", pgptest.TestKeyPublic)
+	form.Add("proof", proof.String())
+
+	response := testRouter.PostForm("/profile/add-pgp-key", form)
+
+	assert.Equal(t, http.StatusSeeOther, response.Result().StatusCode)
+
+	keys, err = testRouter.App.GetUserPGPKeys(user.ID)
+	assert.NotNil(t, keys)
+	assert.Equal(t, 1, len(keys))
+	assert.NoError(t, err)
+
+	key := keys[0]
+	assert.Equal(t, pgptest.TestKeyFingerprint, key.Fingerprint)
 }
--- internal/server/router/routertest/routertest.go
+++ internal/server/router/routertest/routertest.go
@@ -3,6 +3,8 @@ package routertest
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"salsa.debian.org/autodeb-team/autodeb/internal/filesystem"
@@ -31,8 +33,20 @@ func SetupTest(t *testing.T) *RouterTest {
 	return routerTest
 }
 
+//ServeHTTP serves an http request
 func (routerTest *RouterTest) ServeHTTP(request *http.Request) *httptest.ResponseRecorder {
 	response := httptest.NewRecorder()
 	routerTest.Router.ServeHTTP(response, request)
 	return response
+}
+
+//PostForm will post a form
+func (routerTest *RouterTest) PostForm(path string, form *url.Values) *httptest.ResponseRecorder {
+	request := httptest.NewRequest(
+		http.MethodPost,
+		path,
+		strings.NewReader(form.Encode()),
+	)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return routerTest.ServeHTTP(request)
 }

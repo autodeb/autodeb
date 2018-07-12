d36e59d98273810b59436dc8ccdbcfb2ee2511a1
aviau <alexandre@alexandreviau.net>
2018-07-11 23:40:38 -0400

    instructions: document package-upgrades repository

--- internal/server/handlers/webpages/instructions.go
+++ internal/server/handlers/webpages/instructions.go
@@ -7,12 +7,22 @@ import (
 	"salsa.debian.org/autodeb-team/autodeb/internal/server/appctx"
 	"salsa.debian.org/autodeb-team/autodeb/internal/server/handlers/middleware/auth"
 	"salsa.debian.org/autodeb-team/autodeb/internal/server/models"
+	"salsa.debian.org/autodeb-team/autodeb/internal/server/services/jobs"
 )
 
 //InstructionsGetHandler returns a handler for the instructions page
 func InstructionsGetHandler(appCtx *appctx.Context) http.Handler {
 	handlerFunc := func(w http.ResponseWriter, r *http.Request, user *models.User) {
-		renderWithBase(r, w, appCtx, user, "instructions.html", nil)
+
+		data := struct {
+			RepositoryBaseURL         string
+			MainUpgradeRepositoryName string
+		}{
+			RepositoryBaseURL:         appCtx.Config().Aptly.RepositoryBaseURL.String(),
+			MainUpgradeRepositoryName: jobs.MainUpgradeRepositoryName,
+		}
+
+		renderWithBase(r, w, appCtx, user, "instructions.html", data)
 	}
 
 	handler := auth.MaybeWithUser(handlerFunc, appCtx)

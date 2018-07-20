460d3e9bf42aa5abe443833317d03caa9aae65e9
aviau <alexandre@alexandreviau.net>
2018-07-19 23:23:28 -0400

    setup_archive_backport: find out backport candidates

--- internal/worker/jobrunner/exec_setup_archive_backport.go
+++ internal/worker/jobrunner/exec_setup_archive_backport.go
@@ -2,10 +2,15 @@ package jobrunner
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"net/http"
 
 	"salsa.debian.org/autodeb-team/autodeb/internal/errors"
+	"salsa.debian.org/autodeb-team/autodeb/internal/ftpmasterapi"
 	"salsa.debian.org/autodeb-team/autodeb/internal/server/models"
+
+	"pault.ag/go/debian/version"
 )
 
 func (jobRunner *JobRunner) execSetupArchiveBackport(
@@ -17,6 +22,69 @@ func (jobRunner *JobRunner) execSetupArchiveBackport(
 
 	if job.ParentType != models.JobParentTypeArchiveBackport {
 		return errors.Errorf("unsupported parent type %s", job.ParentType)
+	}
+
+	ftpmasterapiClient := ftpmasterapi.NewClient(http.DefaultClient)
+
+	// Get stable packages
+	stablePackages := make(map[string]*ftpmasterapi.Source)
+	pkgs, err := ftpmasterapiClient.GetSourcesInSuite("stable")
+	if err != nil {
+		return errors.WithMessage(err, "could not get unstable sources")
+	}
+	for _, pkg := range pkgs {
+		stablePackages[pkg.Source] = pkg
+	}
+
+	// Get testing packages
+	testingPackages := make(map[string]*ftpmasterapi.Source)
+	pkgs, err = ftpmasterapiClient.GetSourcesInSuite("testing")
+	if err != nil {
+		return errors.WithMessage(err, "could not get testing sources")
+	}
+	for _, pkg := range pkgs {
+		testingPackages[pkg.Source] = pkg
+	}
+
+	// Find backport candidates:
+	// - stable < testing
+	// - in testing but not in stable
+	//
+	// TODO: check dependencies and don't bother trying to build
+	// packages where the dependencies are not satisfied in the
+	// target suite.
+	//
+	var backportCandidates []string
+	for _, testingPackage := range testingPackages {
+
+		stablePackage, ok := stablePackages[testingPackage.Source]
+		if !ok {
+			backportCandidates = append(backportCandidates, testingPackage.Source)
+			fmt.Fprintf(logFile, "Adding %s to backport candidates: not found in stable\n", testingPackage.Source)
+			continue
+		}
+
+		testingVersion, err := version.Parse(testingPackage.Version)
+		if err != nil {
+			return errors.WithMessagef(err, "could not parse version %s", testingPackage.Version)
+		}
+
+		stableVersion, err := version.Parse(stablePackage.Version)
+		if err != nil {
+			return errors.WithMessagef(err, "could not parse version %s", stablePackage.Version)
+		}
+
+		if version.Compare(stableVersion, testingVersion) < 0 {
+			backportCandidates = append(backportCandidates, testingPackage.Source)
+			fmt.Fprintf(
+				logFile,
+				"Adding %s to backport candidates: %s (stable) < %s (testing)\n",
+				testingPackage.Source,
+				stableVersion.String(),
+				testingVersion.String(),
+			)
+		}
+
 	}
 
 	return nil
